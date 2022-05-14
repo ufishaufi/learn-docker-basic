1b9b24bc6b8d8071bc8157befa8470c0e7e4ab60
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-05-14 11:05:39 +0800

    add material docker inspect

---
+++ docker-inspect.sh
@@ -0,0 +1,11 @@
+docker image ls
+
+docker image inspect mongo-express:latest
+
+docker volume ls
+
+docker volume inspect mongorestore
+
+docker container ls
+
+docker container inspect mongodbexpress
